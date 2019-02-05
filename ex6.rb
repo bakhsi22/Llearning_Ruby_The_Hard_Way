8e65fa7ce3c169cbb341400799df8eb056c1ccdf
bakshi22 <bak.shobhit@gmail.com>
2019-02-05 16:23:06 +0530

    feb04

---
+++ ex6.rb
@@ -0,0 +1,15 @@
+# Exercise 6: Strings And Text
+# 
+
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts""
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."

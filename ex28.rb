490a11a4bb4a7d30c996cbe1e403d983f87cacdf
bakshi22 <bak.shobhit@gmail.com>
2019-02-08 15:59:20 +0530

    feb 08

---
+++ ex28.rb
@@ -0,0 +1,26 @@
+# Exercise 28: Boolean Practice
+# trying these equality test in terminal
+# 3 != 4 IS TRUE: TRUE && !("TESTING" != "TEST" || "RUBY" == "RUBY")
+
+
+true && true
+false && true
+1 == 1 && 2 == 1
+"test" == "test"
+1 == 1 || 2 != 1
+true && 1 == 1
+false && 0 != 0
+true || 1 == 1
+"test" == "testing"
+1 != 0 && 2 == 1
+"test" != "testing"
+"test" == 1
+!(true && false)
+!(1 == 1 && 0 != 1)
+!(10 == 1 || 1000 == 1000)
+!(1 != 10 || 3 == 4)
+!("testing" == "testing" && "Zed" == "Cool Guy")
+1 == 1 && (!("testing" == 1 || 1 == 0))
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
+

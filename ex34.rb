74c3f53039c1c3a24f73e4f5ae51229b9e98d2a6
bakshi22 <bak.shobhit@gmail.com>
2019-02-10 23:45:21 +0530

    till_35ex

---
+++ ex34.rb
@@ -0,0 +1,27 @@
+# Exercise 34: Accessing Elements of Arrays
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+a1 = animals[1]
+puts "The animal at 1. => #{a1}"
+
+a3rd = animals[2]
+puts"The third (3rd) animal. => #{a3rd}"
+
+a1st = animals[0]
+puts"The first (1st) animal. => #{a1st}"
+
+a3 = animals[3]
+puts"The animal at 3. => #{a3}"
+
+a5th = animals[4]
+puts"The fifth (5th) animal. => #{a5th}"
+
+a2 = animals[2]
+puts"The animal at 2. => #{a2}"
+
+a6th = animals[5]
+puts"The sixth (6th) animal. => #{a6th}"
+
+a4 = animals[4]
+puts"The animal at 4. => #{a4}"
